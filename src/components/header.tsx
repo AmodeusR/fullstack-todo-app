2eb335f83ab8bf352821858c190af25ad382ce17
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-03-30 00:15:19 -0300

    Create Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between border-b-[1px] border-slate-300 w-full px-8 py-4">
+      <span className="font-bold text-xl text-slate-700">Afazeres</span>
+      <ul className="flex justify-between gap-6">
+        <li className="cursor-pointer transition-colors hover:text-slate-500">
+          início
+        </li>
+        <li className="cursor-pointer transition-colors hover:text-slate-500">
+          sobre
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
