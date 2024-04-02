805b730c8ea93976e411b5510dd5034fc0cefddc
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-04-02 11:39:50 -0300

    Create Form.tsx

---
+++ src/components/Form.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+
+type FormType = {
+  addTodo: (formData: FormData) => Promise<void>;
+}
+
+const Form = ({ addTodo }: FormType) => {
+
+  return (
+    <form action={addTodo} className="flex w-11/12 gap-1 mb-8">
+      <input
+        type="text"
+        className="grow p-2 outline-none border-2 rounded text-sm focus:border-blue-400 transition-colors"
+        name="description"
+      />
+      <button
+        className="text-white font-medium bg-blue-500 px-4 py-2 rounded hover:bg-blue-400 transition-colors focus:bg-blue-400 focus:outline-none"
+      >
+        Adicionar
+      </button>
+    </form>
+  );
+};
+
+export default Form;
