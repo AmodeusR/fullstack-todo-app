dcd8230c6837ee139d1b5ea567db780f26bf98bf
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-04-02 11:40:43 -0300

    Update Main
    Update main with addTodo handler and new components

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,31 @@
-"use client";
-
 import React from "react";
+import Form from "./Form";
+import Todos from "./Todos";
+import { db } from "@/utils/db";
+import { revalidatePath } from "next/cache";
 
-const Main = () => {
+const Main = async () => {
+  const todos = await db.todo.findMany();
+
+  const addTodo = async (formData: FormData) => {
+    "use server";
+    
+    const description = formData.get("description");
+    await db.todo.create({
+      data: {
+        description: description as string
+      }
+    })
+
+    revalidatePath("/todos");
+  }
+
+
   return (
     <main className=" flex flex-col items-center max-w-screen-lg w-full">
       <h2 className="text-center text-2xl font-medium mb-4">O que você tem pra fazer?</h2>
-      <div className="flex w-11/12 gap-1 mb-8">
-        <input type="text" className="grow p-2 outline-none border-2 rounded text-sm focus:border-blue-400 transition-colors" />
-        <button type="button" className="text-white font-medium bg-blue-500 px-4 py-2 rounded hover:bg-blue-400 transition-colors focus:bg-blue-400 focus:outline-none">Adicionar</button>
-      </div>
-      <ul>
-        <li>Lorem ipsum dolor sit amet.</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-        <li>Lorem, ipsum.</li>
-      </ul>
+      <Form addTodo={addTodo} />
+      <Todos todos={todos} />
     </main>
   );
 };
