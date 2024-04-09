aab279d0574b7fd96de0bf4d9d98cf522b4af7f1
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-04-09 18:29:27 -0300

    Update Form.tsx
    Remove typing for addTodo server action and import it from actions file and change button for Button.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,10 @@
 "use client";
 
+import { addTodo } from "@/utils/actions";
 import React, { useRef } from "react";
+import Button from "./Button";
 
-type FormType = {
-  addTodo: (formData: FormData) => Promise<void>;
-}
-
-const Form = ({ addTodo }: FormType) => {
+const Form = () => {
   const formRef = useRef<HTMLFormElement>(null);
 
   return (
@@ -19,11 +17,7 @@ const Form = ({ addTodo }: FormType) => {
         className="grow p-2 outline-none border-2 rounded text-sm focus:border-blue-400 transition-colors"
         name="description"
       />
-      <button
-        className="text-white font-medium bg-blue-500 px-4 py-2 rounded hover:bg-blue-400 transition-colors focus:bg-blue-400 focus:outline-none"
-      >
-        Adicionar
-      </button>
+      <Button />
     </form>
   );
 };
