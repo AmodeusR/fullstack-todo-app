2b831a6bcaf03d5342daec206dbc6e40becf48d4
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-08-09 18:30:34 -0300

    Fix route types

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,10 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request, res: Response) {
   const todos = await prisma.todo.findMany()
 
   return NextResponse.json({todos})
